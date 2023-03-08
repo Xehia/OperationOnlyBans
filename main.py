83001935da1ae5efa21e77f799f7aec63192fd30
Xehia <matteoperini01@gmail.com>
2023-03-08 13:12:36 +0100

    First commit 🎉

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+ServerPath = "E:\Coding\OperationOnlyBans/HarshDoorstop"
+
+if os.name == 'nt':
+    serverOs = "WindowsServer"
+else:
+    serverOs = "LinuxServer"
+
+def CheckPath():
+    if not os.path.isdir(ServerPath):
+        print("Server folder not found!")
+        exit()
+
+    if not os.path.exists(ServerPath + f"/Saved/Config/{serverOs}/Bans.cfg"):
+        os.makedirs(ServerPath+f"/Saved/Config/{serverOs}", exist_ok=True)
+
+
+def bannedPlayers():
+    response = requests.get(url="https://raw.githubusercontent.com/PERPGamer/OHD-Communtiy-Ban-List/main/Bans.cfg")
+    return (response.content).decode("utf-8").split("\n")
+
+CheckPath()
+
+f = open(ServerPath + f"/Saved/Config/{serverOs}/Bans.cfg", "w")
+for player in bannedPlayers():
+    f.write(player + "\n")  
+f.close()
+
+print("File Bans.cfg updated succesfully!")
